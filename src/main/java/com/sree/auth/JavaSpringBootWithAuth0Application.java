6a08f122d373e95dcb84541c80c8b3d25b9ff6c3
SreekanthGaddoju <93276331+SreekanthGaddojuMsx@users.noreply.github.com>
2024-05-07 15:21:55 +0200

    Spring Boot Java 17 Application Integrated with Auth0 Authentication.

---
+++ src/main/java/com/sree/auth/JavaSpringBootWithAuth0Application.java
@@ -0,0 +1,13 @@
+package com.sree.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootWithAuth0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBootWithAuth0Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sree/auth/JavaSpringBootWithAuth0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sree.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootWithAuth0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
